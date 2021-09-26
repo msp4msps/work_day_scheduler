4bd964c23426967aaec447998e58ce4c7eb4211a
msp4msps <msp4msps@tminus365.com>
2021-09-26 07:57:16 -0600

    Color changes with time

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
+//Set Variables
 var current = document.getElementById("currentDay");
+var hour = document.querySelectorAll(".description");
+console.log(hour);
 
-console.log(current);
-
+//Add Current Date upon page load
 current.textContent = moment().format("dddd, MMMM Do");
+
+function checkTime() {
+  for (i = 0; i < hour.length; i++) {
+    //Set Format
+    var format = "hh:mm";
+    //Convert String to Number
+    var hourToNum = parseInt(hour[i].id);
+    //Compensate for Timespan
+    var nextHour = hourToNum + 0.59;
+    //Set Start and End Time
+    var startTime = moment(`0${hour[i].id}`, format);
+    var EndTime = moment(`0${nextHour}`, format);
+    console.log(startTime);
+    console.log(EndTime);
+    //Determine Background Color
+    var currentTime = moment();
+    if (currentTime.isBetween(startTime, EndTime)) {
+      console.log("tooooo");
+      hour[i].setAttribute("class", " col-10 present description");
+    } else if (currentTime < EndTime) {
+      console.log("THis is working");
+      hour[i].setAttribute("class", "col-10 future description");
+    } else {
+      hour[i].setAttribute("class", "col-10 past description");
+    }
+  }
+}
+checkTime();
